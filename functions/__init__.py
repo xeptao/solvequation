af74e5f0a47a21fafdf335ca5fbe34c224e3f2d4
Xeptao <xeptaowastaken@gmail.com>
2023-07-19 17:49:23 +0300

    feat: validator

---
+++ functions/__init__.py
@@ -0,0 +1,1 @@
+from .validate_eqn import validate_eqn
---
+++ functions/validate_eqn.py
@@ -0,0 +1,86 @@
+from classes import Error
+
+
+def validate_eqn(eqn: str, var: str):
+    allowed = ["+", "-" "/", "*", "^", "=", var]
+    error = Error(eqn, var)
+    equal_count = 0
+    errors = {
+        "input": "InputError",
+        "logic": "LogicError",
+        "syntax": "SyntaxError",
+    }
+    common_messages = [lambda a, b: f"{a} and {b} can't be neighbours"]
+
+    if "=" not in eqn:
+        error.throw(errors["logic"], "An equation can't be made without an =")
+
+    if var not in eqn:
+        error.throw(
+            errors["input"], f"Your equation needs to have {var} in it"
+        )
+
+    if len(var) != 1:
+        error.throw(
+            errors["input"],
+            f"Your variable needs to be one character",
+        )
+
+    for i, letter in enumerate(eqn):
+        is_last_letter = i == len(eqn) - 1
+        is_first_letter = i == 0
+
+        if not letter.isdigit() and letter not in allowed:
+            error.throw(errors["input"], f"{letter} isn't allowed")
+
+        if letter == "=":
+            if is_last_letter or is_first_letter:
+                error.throw(
+                    errors["logic"],
+                    "An equation can't be formed if there isn't two sides",
+                )
+
+            equal_count += 1
+
+        if equal_count > 1:
+            error.throw(errors["logic"], "There can't be multiple '='s")
+
+        if not is_first_letter:
+            prev = eqn[i - 1]
+
+            if (
+                letter == var
+                and prev.isdigit()
+                or letter.isdigit()
+                and prev == var
+            ):
+                error.throw(
+                    errors["syntax"],
+                    common_messages[0](letter, prev),
+                )
+
+            if letter in allowed and prev in allowed:
+                if letter != var and prev != var:
+                    if letter != "=" and prev != "=":
+                        error.throw(
+                            errors["syntax"],
+                            common_messages[0](letter, prev),
+                        )
+
+                if letter == prev:
+                    error.throw(
+                        errors["syntax"],
+                        common_messages[0](letter, prev),
+                    )
+
+    # placed at end because unpack would give an error if there were more
+    # than one equal signs
+    left, right = eqn.split("=")
+
+    if left == right:
+        error.throw(
+            errors["logic"], "Nothing to compute since both sides are equal"
+        )
+
+    if left == var or right == var:
+        error.throw(errors["logic"], "This equation has already been solved")
