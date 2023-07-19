e75e8df6992dc9bf61b8a5997b94895c43be75a7
Xeptao <xeptaowastaken@gmail.com>
2023-07-19 15:29:33 +0300

    feat: error class

---
+++ classes/Error.py
@@ -0,0 +1,9 @@
+class Error:
+    def __init__(self, eqn: str, var: str) -> None:
+        self.var = var
+        self.eqn = eqn 
+    
+    def throw(self, error_type, error_msg):
+        print(f"\033[91m{error_type}\033[0m: {error_msg}")
+
+        exit(1)
