34c1f4f54dfac103c69e64223cf81a567b718354
Xeptao <xeptaowastaken@gmail.com>
2023-07-19 18:53:30 +0300

    feat: turn eqn into operations

--- script.py
+++ script.py
@@ -1,10 +1,35 @@
 from functions import validate_eqn
 
+
 def solve(eqn: str, var: str) -> str:
     eqn = eqn.replace(" ", "")
     result = ""
+    token_symbols = ["+", "-", "/", "*", "^", "="]
+    token_names = ["PLUS", "MINUS", "DIV", "MUL", "EXP", "EQUAL"]
+    operations = []
+    curr_num = ""
 
     validate_eqn(eqn, var)
+
+    for i, letter in enumerate(eqn):
+        if letter.isdigit() or letter == var:
+            curr_num += letter
+
+            if i != len(eqn) - 1:
+                next = eqn[i + 1]
+
+                if not next.isdigit():
+                    operations.append(curr_num)
+                    curr_num = ""
+            else:
+                operations.append(curr_num)
+
+            continue
+
+        if letter in token_symbols:
+            tok_name_idx = token_symbols.index(letter)
+
+            operations.append(token_names[tok_name_idx])
 
     return result
 
