a9f2ed65fac72997e48ce99df7b4fc512353395e
Xeptao <xeptaowastaken@gmail.com>
2023-07-20 10:53:34 +0300

    style: removing trailing whitespace

--- classes/Error.py
+++ classes/Error.py
@@ -1,9 +1,9 @@
 class Error:
     def __init__(self, eqn: str, var: str):
         self.var = var
-        self.eqn = eqn 
-    
-    def throw(self, error_type, error_msg):
+        self.eqn = eqn
+
+    def throw(self, error_type: str, error_msg: str):
         print(f"\033[91m{error_type}\033[0m: {error_msg}")
 
         exit(1)
