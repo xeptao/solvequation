286701c534c0f392b464fe5479e3bad8bcc1d0ce
Xeptao <xeptaowastaken@gmail.com>
2023-07-20 11:22:49 +0300

    refactor: code according to pylint

--- functions/validate_eqn.py
+++ functions/validate_eqn.py
@@ -1,7 +1,23 @@
+"""
+This is the module for the validate_eqn functions and its helpers which
+check if the logic, syntax, and input are correct.
+"""
+
 from classes import Error
 
 
 def all_are_true(inputs: tuple, *conditions):
+    """
+    Checks if all given conditions are true for all inputs.
+
+            Parameters:
+                inputs (tuple): The inputs to apply the conditions for
+                conditions: The conditions to apply to the inputs
+
+            Returns:
+                False if there are any Falses, else True
+    """
+
     results = []
 
     for condition in conditions:
@@ -11,11 +27,34 @@ def all_are_true(inputs: tuple, *conditions):
 
 
 def any_is_true(inputs: tuple, condition):
+    """
+    Checks if the given condition is true for any of the inputs.
+
+            Parameters:
+                inputs (tuple): The inputs to apply the conditions for
+                condition: The condition to apply to the inputs
+
+            Returns:
+                True if there are any Trues, else False
+    """
+
     return any(condition(input) for input in inputs)
 
 
 def validate_eqn(eqn: str, var: str):
-    allowed = ("+", "-" "/", "*", "^", "=", var)
+    """
+    Checks if the logic, syntax, and the inputs for the
+    variable and equation are correct.
+
+            Parameters:
+                inputs (tuple): The inputs to apply the conditions for
+                condition: The condition to apply to the inputs
+
+            Returns:
+                True if there are any Trues, else False
+    """
+
+    allowed = ("+", "-", "/", "*", "^", "=", var)
     error = Error(eqn, var)
     equal_count = 0
     tags = {
@@ -23,15 +62,7 @@ def validate_eqn(eqn: str, var: str):
         "logic": "LogicError",
         "syntax": "SyntaxError",
     }
-    common_messages = [lambda a, b: f"{a} and {b} can't be neighbours"]
-
-    if "=" not in eqn:
-        error.throw(tags["logic"], "An equation can't be made without an =")
-
-    if var not in eqn:
-        error.throw(
-            tags["input"], f"Your equation needs to have {var} in it"
-        )
+    common_messages = [lambda a, b: f"{a} and {b} can't be next to eachother"]
 
     if len(var) != 1:
         error.throw(
@@ -39,26 +70,32 @@ def validate_eqn(eqn: str, var: str):
             "Your variable needs to be one character",
         )
 
+    if any_is_true(("=", var), lambda x: x not in eqn):
+        error.throw(
+            tags["input"],
+            f"Your equation either doesn't have '=' or it doesn't have {var}",
+        )
+
     for i, letter in enumerate(eqn):
-        is_last_letter = i == len(eqn) - 1
-        is_first_letter = i == 0
+        is_last = i == len(eqn) - 1
+        is_first = i == 0
 
         if not letter.isdigit() and letter not in allowed:
             error.throw(tags["input"], f"{letter} isn't allowed")
 
         if letter == "=":
-            if is_last_letter or is_first_letter:
+            equal_count += 1
+
+            if is_last or is_first:
                 error.throw(
                     tags["logic"],
                     "An equation can't be formed if there isn't two sides",
                 )
 
-            equal_count += 1
+            if equal_count > 1:
+                error.throw(tags["logic"], "There can't be multiple '='s")
 
-        if equal_count > 1:
-            error.throw(tags["logic"], "There can't be multiple '='s")
-
-        if not is_first_letter:
+        if not is_first:
             prev = eqn[i - 1]
 
             if (
